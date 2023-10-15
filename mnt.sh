56fdaa6a88327243edf44f98b85ce14a403b3f18
dontexit <edwinhamal@gmail.com>
2023-10-15 10:01:32 +0545

    forgive me father for i didnt know

--- mnt.sh
+++ mnt.sh
@@ -1,4 +1,4 @@
 #bin bash
-sudo mkdir -p /run/media/edwin/Edwin
-sudo mount -t ntfs3 /dev/nvme0n1p5 /run/media/edwin/Edwin
+sudo mkdir -p /mnt/Edwin
 sudo ntfsfix -b -d /dev/nvme0n1p5 
+sudo mount -t ntfs3 /dev/nvme0n1p5 /mnt/Edwin
