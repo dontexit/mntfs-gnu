45a8b58edf888b8827aade558b170d94897368e0
dontexit <edwinhamal@gmail.com>
2023-11-20 19:04:39 +0545

    windows partition too

--- mnt.sh
+++ mnt.sh
@@ -1,4 +1,8 @@
 #bin bash
 sudo mkdir -p /mnt/Edwin
+sudo ntfsfix -b -d /dev/nvme0n1p6 
+sudo mount -t ntfs3 /dev/nvme0n1p6 /mnt/Edwin
+
+sudo mkdir -p /mnt/Windows
 sudo ntfsfix -b -d /dev/nvme0n1p5 
-sudo mount -t ntfs3 /dev/nvme0n1p5 /mnt/Edwin
+sudo mount -t ntfs3 /dev/nvme0n1p5 /mnt/Windows
